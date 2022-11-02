180bca99987ccf92c36c3ae3f5579d93ef881965
Yusuf <ogvenomassassin@gmail.com>
2022-11-02 12:49:14 -0800

    promise

--- apps.js
+++ apps.js
@@ -1,7 +1,11 @@
-function oppositeBoolean(bool){
-    if (bool === true)
-    return false
-    if (bool === false)
-    return true
+function filterOutFalsy(arr) {
+    let truthyArr = []
+    for (let i = 0; i < arr.length; ++i) {
+        if (!!arr[i] === true) {
+            truthyArr.push(arr[i])
+        }
+    }
+    return truthyArr
 }
-console.log(oppositeBoolean(false))
+
+console.log(filterOutFalsy([``, NAN, 500, null, false, 0]))
